175f88f1174ee544baabd0421050b9dd2315c0dd
bobi404 <janardhanaabby04@gmail.com>
2023-09-15 11:59:32 +0800

    style : adding code for template footer

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,8 @@
 <?php
 
 define('DB_USER','root');
-define('DB_PASS','danareksa12');
+define('DB_PASS','');
 define('DB_HOST','localhost');
 define('DB_NAME','taksu_rent');
-define('DB_PORT','3306');
 
 define('BASE_URL','http://localhost/TaksuRent/public');
