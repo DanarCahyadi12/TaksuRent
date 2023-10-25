47a9136e5a7c7e81a1355444b640ff480fa47d88
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-10-25 15:52:52 +0800

    feat: 404 not found

---
+++ app/controllers/NotFound.php
@@ -0,0 +1,9 @@
+<?php 
+class NotFound extends Controller {
+    public function index() {
+        $datas['title'] = '404 not found';
+        $this->view('templates/header',$datas);
+        $this->view('notfound');
+        $this->view('templates/footer');
+    }
+}
