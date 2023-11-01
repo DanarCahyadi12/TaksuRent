990f7558176f7480d5b0f55e59d4abc354de8a3e
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-11-01 18:05:14 +0800

    chore: fix structure folder login view

--- app/controllers/Login_penyewa.php
+++ app/controllers/Login_penyewa.php
@@ -4,7 +4,7 @@ class Login_penyewa extends Controller {
     public function index() {
         $datas['title'] = "Login sebagai penyewa";
         $this->view('templates/header',$datas);
-        $this->view('penyewa/login');
+        $this->view('penyewa/login/login');
         $this->view('templates/footer');
     }
 
