993793a5fff35ad9ca5e331dedd4db6d09947236
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-11-02 14:15:23 +0800

    add profile at navbar

--- app/views/partials/navbar.php
+++ app/views/partials/navbar.php
@@ -15,4 +15,21 @@
       </ul>
     </div>
   </div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <?php if(!Session::exits('user')): ?>
+            <a class="nav-link active" aria-current="page" href="<?= url('login-penyewa'); ?>">Login</a>
+          <?php endif ?>
+          <?php if(Session::exits('user')): ?>
+            <div class='d-flex align-content-center'>
+              <a class="navbar-brand" aria-current="page" href="<?= url('profile'); ?>">
+                <img src="images/img.jpg" class="rounded-circle" style="width: 40px; height:40px;"alt="Avatar" />
+              </a>
+              <a class="nav-link active" aria-current="page" href="<?= url('logout'); ?>">Logout</a>
+            </div>
+          <?php endif ?>
+        </li>
+      </ul>
+    </div>
 </nav>
