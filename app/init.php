def8857a630f7dd9149129a4b680be77b5e8fdfd
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-11-04 18:30:17 +0800

    Initial Authorization.php

--- app/init.php
+++ app/init.php
@@ -2,7 +2,7 @@
 
 require_once "core/App.php";
 require_once "core/Database.php";
-require_once "core/Guard.php";
+require_once "core/Authorization.php";
 require_once "core/Session.php";
 require_once "core/Flasher.php";
 require_once "core/Redirect.php";
